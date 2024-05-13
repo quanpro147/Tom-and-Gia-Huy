eccdee34a6e3da9fc5c56ab085caea845d0d7b64
Phan Ngoc Quan <quanphanpq147@gmail.com>
2024-05-13 16:49:10 +0700

    complete saveload

--- Do_an/test.py
+++ Do_an/test.py
@@ -1,5 +1,8 @@
 from game import *
 
+# saveloadmanager = saveloadsystem('.save', 'Do_an/SaveLoad/game_manager')
+# saveloadmanager.delete_file('quan1')
+
 game = Game()
 game.load('quan1')
 game.run()
