c539d83a5045d308c6d1705043d73d73fdb7e24c
Phan Ngoc Quan <quanphanpq147@gmail.com>
2024-05-19 13:55:11 +0700

    nothing

--- Do_an/cell.py
+++ Do_an/cell.py
@@ -15,14 +15,22 @@ class cell():
     def draw(self, screen, cell_size):
         """ This function use to draw wall if it appear """
         x, y = self.x * cell_size, self.y * cell_size 
+        # wall_ver = pygame.image.load('Do_an/Assets/Wall/wall_vertical.png')
+        # wall_ver = pygame.transform.scale(wall_ver, (cell_size, 3 + cell_size//20))
+        # wall_hor = pygame.image.load('Do_an/Assets/Wall/wall_horizontal.png')
+        # wall_hor = pygame.transform.scale(wall_hor, (3 + cell_size//20, cell_size))
         if self.walls['top']:
             pygame.draw.line(screen, pygame.Color('black'), (x, y), (x + cell_size, y), self.thickness + cell_size//20)
+            # screen.blit(wall_ver, (x, y))
         if self.walls['right']:
             pygame.draw.line(screen, pygame.Color('black'), (x + cell_size, y), (x + cell_size, y + cell_size), self.thickness + cell_size//20)
+            # screen.blit(wall_hor, (x + cell_size, y))
         if self.walls['bot']:
             pygame.draw.line(screen, pygame.Color('black'), (x + cell_size, y + cell_size), (x , y + cell_size), self.thickness + cell_size//20)
+            # screen.blit(wall_ver, (x, y + cell_size))
         if self.walls['left']:
             pygame.draw.line(screen, pygame.Color('black'), (x, y + cell_size), (x, y), self.thickness + cell_size//20)
+            # screen.blit(wall_hor, (x, y))
     
     def remove_walls(self, neighbour_row, neighbour_col):
         """
